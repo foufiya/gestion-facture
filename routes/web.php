e2b1f7d3c62e5e1849699b5facac519efe3ddbd2
foufiya <wafaelole2003@gmail.com>
2024-02-28 14:18:58 +0100

    setup auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::Routes();
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', [DashboardController::class, "index"]);
+    
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
